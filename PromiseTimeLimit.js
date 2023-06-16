89e404ffcc6289a00517da1a3dfec4f8d3ad0fa5
Fran <kezicfran@gmail.com>
2023-06-16 15:04:38 +0300

    first commit

---
+++ PromiseTimeLimit.js
@@ -0,0 +1,38 @@
+// Usage: 
+// 
+// function fn(t) {
+//     return new Promise(res => setTimeout(res, t))
+// }
+// 
+// const limited = timeLimit(fn, 100)
+// 
+// limited(150).catch(console.log)
+// 
+
+const timeLimit = (fn, t) => {
+    return async function(...args) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => reject("Time limit exceeded"), t)
+
+            fn(...args)
+                .then((res) => resolve(res))
+                .catch((e) => reject(e))
+        })
+    }
+}
+
+const awaitTimeLimit = (fn, t) => {
+    return async function(...args) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => reject("TLE"), t)
+
+            try {
+                const res = await fn(...args)
+                resolve(res)
+            } catch (error) {
+                reject(error)
+            }
+            
+        })
+    }
+}
